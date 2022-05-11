2edcdfdf9076e4a5ecb610fbc78b662b67db3a2c
Stevan Olegario <stevan.olegario@gmail.com>
2022-05-11 17:22:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: {}
+  }
+
+componentDidMount() {
+  fetch('http://localhost:8895/v0.1/transactions/hw?id=2151060662&amount=8523.0&transaction_date=2022-05-09&card_last4=7580')
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+    this.setState({ contacts: data })
+  })
+  .catch(console.log)
+}
+  render() {
+    return (
+      <div className="wrapper">
+        <form>
+        <fieldset>
+          <label>
+              <p>Transaction Id:</p>
+            <input type="text" name="id" />
+          </label>
+          </fieldset>
+          <fieldset>
+          <label>
+            <p>Transaction Data:</p>
+            <input type="text" name="transaction_date" />
+          </label>
+          </fieldset>
+
+          <fieldset>
+          <label>
+            <p>Amount:</p>
+            <input type="text" name="amount" />
+          </label>
+          </fieldset>
+
+          <fieldset>
+          <label>
+            <p>Card last 4:</p>
+            <input type="text" name="card_last4" />
+          </label>
+          </fieldset>
+         <button type="submit">Submit</button>
+        </form>
+        <div className="dataloader">
+          <p>{this.state.contacts.name}</p>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+
+            <h5 class="card-title">Merchant Name:</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{this.state.contacts.username}</h6>
+
+            <h5 class="card-title">Transaction Date:</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{this.state.contacts.timestamp}</h6>
+
+
+            <h5 class="card-title">Transaction Time:</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{this.state.contacts.timestamp}</h6>
+
+            <h5 class="card-title">Merchant Type:</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{this.state.contacts.username}</h6>
+
+            <h5 class="card-title">Merchant Location:</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{this.state.contacts.id}</h6>
+          </div>
+
+
+
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default App;
